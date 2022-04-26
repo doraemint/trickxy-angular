dba6ecbc00bb4ab42edc09605bfc6e5195d7b2a3
doraemint <doraemintsmile@hotmail.com>
2022-04-26 11:06:38 +0700

    hide create blog button when not login

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IBlog } from 'src/app/models/blog';
+import { AuthService } from 'src/app/services/auth.service';
 import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
@@ -9,22 +10,25 @@ import { BlogService } from 'src/app/services/blog.service';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
-  allBlog :IBlog[] = [];
+  allBlog: IBlog[] = [];
+  myData: any;
   constructor(
     private router: Router,
-    private blogService: BlogService
+    private blogService: BlogService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
     this.getAllBlog();
+    this.myData = this.authService.getDataFromLocalStorage();
   }
 
   getAllBlog() {
-  this.blogService.getAllBlog().subscribe((data)=>{
-    this.allBlog = data;
-    console.log(this.allBlog)
-  });
-   //
+    this.blogService.getAllBlog().subscribe((data) => {
+      this.allBlog = data;
+      console.log(this.allBlog)
+    });
+    //
   }
   goToCreatePost() {
     this.router.navigate(['/create-edit-blog'])
